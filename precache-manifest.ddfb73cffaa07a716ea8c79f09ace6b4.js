a0b6569caf8d77929569938a9ef5811928ad6076
Sowjanya Ennam <sowjanyaennam95@gmail.com>
2020-04-03 19:56:40 +0530

    Updates

---
+++ precache-manifest.ddfb73cffaa07a716ea8c79f09ace6b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46d5da00b0cf81aa831c224a3e600aa",
+    "url": "/redbaton-shoppingcart/index.html"
+  },
+  {
+    "revision": "24ac26d52a1780975996",
+    "url": "/redbaton-shoppingcart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6fa1ee9fab4f070e3ec",
+    "url": "/redbaton-shoppingcart/static/js/2.0d32ef27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redbaton-shoppingcart/static/js/2.0d32ef27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ac26d52a1780975996",
+    "url": "/redbaton-shoppingcart/static/js/main.6b4c6e34.chunk.js"
+  },
+  {
+    "revision": "7ec6a6a44f651630f10b",
+    "url": "/redbaton-shoppingcart/static/js/runtime-main.92477dbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redbaton-shoppingcart/static/media/logo.5d5d9eef.svg"
+  }
+]);
